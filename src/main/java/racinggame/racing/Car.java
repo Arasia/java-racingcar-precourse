bbe2724e1aff6d2e5c89d40ab796a5f8ed9e4f88
Arasia <rlacndtjs261@gmail.com>
2021-10-11 03:55:21 +0900

    refactor: Car의 고정 값 상수화

--- src/main/java/racinggame/racing/Car.java
+++ src/main/java/racinggame/racing/Car.java
@@ -3,7 +3,10 @@ package racinggame.racing;
 import nextstep.utils.Randoms;
 
 public class Car {
+    private static final int MAX_LENGTH_OF_NAME = 5;
     private static final int MIN_NUMBER_FOR_MOVE = 4;
+    private static final int MIN_NUMBER_FOR_RANDOM = 0;
+    private static final int MAX_NUMBER_FOR_RANDOM = 9;
 
     private final String name;
     private int position = 0;
@@ -19,13 +22,13 @@ public class Car {
             throw new IllegalArgumentException("[ERROR] 이름은 반드시 입력되어야합니다.");
         }
 
-        if (name.length() > 5) {
+        if (name.length() > MAX_LENGTH_OF_NAME) {
             throw new IllegalArgumentException("[ERROR] 이름의 길이는 5자 이하만 가능합니다.");
         }
     }
 
     public void tryToMove() {
-        int randomNum = Randoms.pickNumberInRange(0, 9);
+        int randomNum = Randoms.pickNumberInRange(MIN_NUMBER_FOR_RANDOM, MAX_NUMBER_FOR_RANDOM);
 
         if (MIN_NUMBER_FOR_MOVE <= randomNum) {
             position++;
